a64aa0d2f7bba423f154a1fe8739e1fe6eec590b
krisztian <alt.krisztian@gmail.com>
2018-03-23 01:52:17 +0100

    Create services for user registration and authentication process

--- src/main/java/com/lastnews/service/UserService.java
+++ src/main/java/com/lastnews/service/UserService.java
@@ -1,4 +1,13 @@
 package com.lastnews.service;
 
+import com.lastnews.model.User;
+
 public interface UserService {
+
+    String registerUser(User user);
+
+    User findByEmail(String email);
+
+    String userActivation(String activationCode);
+
 }
--- src/main/java/com/lastnews/service/UserServiceImpl.java
+++ src/main/java/com/lastnews/service/UserServiceImpl.java
@@ -1,4 +1,34 @@
 package com.lastnews.service;
 
-public class UserServiceImpl {
+import com.lastnews.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+
+
+
+    @Override
+    public String registerUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public String userActivation(String activationCode) {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
