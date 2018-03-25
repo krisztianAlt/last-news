deb7f9285e78f78c82b7c9c5f3367551285b8018
krisztian <alt.krisztian@gmail.com>
2018-03-25 21:57:41 +0200

    Show news in modal - not perfect yet, extra button needed

--- src/main/resources/public/js/europe.js
+++ src/main/resources/public/js/europe.js
@@ -33,7 +33,7 @@ app.europeMap = {
             success: function(response) {
                 var package = JSON.parse(response.answer);
                 var news = package.articles;
-                app.europeMap.listNews(news);
+                app.europeMap.listNews(news, countryName);
             },
             error: function() {
                 console.log('ERROR: API calling failed.');
@@ -41,8 +41,50 @@ app.europeMap = {
         });
     },
 
-    listNews: function (news) {
+    listNews: function (news, countryName) {
+        console.log(countryName);
+        var modalTitle = document.getElementById("newsModalLabel");
+        modalTitle.textContent = "News about " + countryName;
 
+        // delete previous news table content:
+        var deleteNewsRows = document.getElementsByClassName('news-table-row');
+        while (deleteNewsRows.length > 0) {
+            deleteNewsRows[0].remove();
+        }
+
+        // put data into the modal:
+        var newsTable = document.getElementById('news-table-body');
+        for (newsIndex = 0; newsIndex < news.length; newsIndex++) {
+
+            var newRow = document.createElement('tr');
+            newRow.className = 'news-table-row';
+
+            var title = document.createElement('td');
+            var titleText = document.createTextNode(news[newsIndex].title);
+            title.appendChild(titleText);
+
+            var author = document.createElement('td');
+            var authorText = document.createTextNode(news[newsIndex].author);
+            author.appendChild(authorText);
+
+            var link = document.createElement('td');
+            var anchor = document.createElement('a');
+            document.createElement('href');
+            var linkText = document.createTextNode("Read");
+            anchor.appendChild(linkText);
+            anchor.target = "_blank";
+            anchor.title = "Read";
+            anchor.href = news[newsIndex].url;
+            link.appendChild(anchor);
+
+            // todo: add picture
+
+            newRow.appendChild(title);
+            newRow.appendChild(author);
+            newRow.appendChild(link);
+
+            newsTable.appendChild(newRow);
+        }
     }
 
 };
